e93e6d9cf4c0bbdb987ba373336146927ddf55d1
Abdulmalik Adebayo <milikiadbay@gmail.com>
2023-02-03 22:43:59 +0100

    feat: Navigate Programmatically using useNavigate hook

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 const Homepage = props => {
+  const navigate = useNavigate()
+
+  const navigateHandler = () => {
+    navigate('/products')
+  }
   return (
     <div>
       <h1>My Homepage</h1>
       <p>Go to the <Link to="/products">list of products</Link></p>
+      <button  onClick={navigateHandler}>Navigate to Products</button>
     </div>
   )
 }
