c921573c470b14560629909ca5355a1b86881d9a
Abdulmalik Adebayo <milikiadbay@gmail.com>
2023-02-04 22:04:40 +0100

    Add Link for Dynamic Paths

--- src/Pages/Productpage.jsx
+++ src/Pages/Productpage.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-const Productpage = props => {
+const Productpage = (props) => {
+  const PRODUCTS = [
+    { id: 'p1', title: 'Book 1', price: 6 },
+    { id: 'p2', title: 'Book 2', price: 6 },
+    { id: 'p3', title: 'Book 3', price: 6 },
+    { id: 'p4', title: 'Book 4', price: 6 },
+    { id: 'p5', title: 'Book 5', price: 6 },
+  ];
+
   return (
     <div>
       <h1>My Productpage</h1>
+      <ul>
+        {PRODUCTS.map((product) => {
+          return (
+            <li key={product.id}>
+              <Link to={`/products/${product.id}`}>{product.title}</Link>
+            </li>
+          );
+        })}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-Productpage.propTypes = {}
+Productpage.propTypes = {};
 
-export default Productpage
+export default Productpage;
